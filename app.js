0274eb912d161d2f7aa9d7fd0f000fc8a6bfcf45
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2023-08-30 21:00:25 +0100

    Create base grid structure

---
+++ app.js
@@ -0,0 +1,30 @@
+function drawGrid(numberOfElements) {
+  const grid = document.querySelector('.grid');
+  grid.setAttribute(
+    'style',
+    `grid-template-rows: repeat(${numberOfElements}, 1fr); grid-template-columns: repeat(${numberOfElements}, 1fr)`
+  );
+  deleteGridElements();
+  createGridElements(grid, numberOfElements);
+}
+
+function deleteGridElements() {
+  const elements = document.querySelectorAll('.grid-element');
+  while (elements.length > 0) {
+    elements.item[0].remove;
+  }
+}
+
+function createGridElements(grid, numberOfElements) {
+  for (let index = 0; index < numberOfElements * numberOfElements; index++) {
+    grid.appendChild(createGridElement());
+  }
+}
+
+function createGridElement() {
+  const element = document.createElement('div');
+  element.classList.add('grid-element');
+  return element;
+}
+
+drawGrid(16);
