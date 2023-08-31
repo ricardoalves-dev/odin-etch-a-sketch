df6e3b9e4413d3e16334b0dd8562b759970403ed
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2023-08-31 13:52:49 +0100

    Create grid size option button

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+const initialValue = 25;
+
 function drawGrid(numberOfElements) {
   const grid = document.querySelector('.grid');
+  deleteGridElements(grid);
   grid.setAttribute(
     'style',
     `grid-template-rows: repeat(${numberOfElements}, 1fr); grid-template-columns: repeat(${numberOfElements}, 1fr)`
   );
-  deleteGridElements();
-  createGridElements(grid, numberOfElements);
+  createGridElements(grid, numberOfElements * numberOfElements);
 }
 
-function deleteGridElements() {
-  const elements = document.querySelectorAll('.grid-element');
-  while (elements.length > 0) {
-    elements.item[0].remove;
+function deleteGridElements(grid) {
+  while (grid.childElementCount > 0) {
+    grid.removeChild(grid.children[0]);
   }
 }
 
 function createGridElements(grid, numberOfElements) {
-  for (let index = 0; index < numberOfElements * numberOfElements; index++) {
+  for (let index = 0; index < numberOfElements; index++) {
     grid.appendChild(createGridElement());
   }
 }
@@ -33,4 +34,16 @@ function changeElementColor(event) {
     this.style.backgroundColor === 'black' ? 'white' : 'black';
 }
 
-drawGrid(16);
+function changeRangeValue(event) {
+  document.querySelector(
+    '.range-label'
+  ).textContent = `${this.value} x ${this.value}`;
+  drawGrid(this.value);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#range').addEventListener('input', changeRangeValue);
+  document.querySelector('#range').dispatchEvent(new Event('click'));
+});
+
+drawGrid(initialValue);
